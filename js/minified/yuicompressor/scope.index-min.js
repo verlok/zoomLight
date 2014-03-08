4863ad5762160b9e61affcdd32c09ba286e3c62a
Andrea Verlicchi <andrea.verlicchi@gmail.com>
2014-03-08 10:13:46 +0100

    Revert 342aef8..d3117d9
    This rolls back to commit 342aef8caa86d29391d0e2de9a1ed9a19c6bf347.

--- js/minified/yuicompressor/scope.index-min.js
+++
@@ -1,1 +0,0 @@
-$(function(){console.group("Scope Index started!");try{var a={openLayer:["#mainImage img","#mainImage button","#container h1"]};pool($("body"),"zoomLight",a);pool($("#test"),"testPlugin",{})}catch(b){console.error("Scope Index error!");console.error(b)}console.groupEnd()});
